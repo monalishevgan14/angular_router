0a370155c17ed38936d8bd19eefe9f085cb5c57f
Monali Shevgan <monali.shevgan@onegreendiary.com>
2025-08-22 17:26:39 +0530

    angular router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'home',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'contact',component:ContactusComponent},
+    {path: '**', redirectTo: 'home' }, // Fallback for unexpected URLs
+    
+];
---
+++ src/app/contactus/contactus.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactus',
+  imports: [],
+  templateUrl: './contactus.component.html',
+  styleUrl: './contactus.component.css'
+})
+export class ContactusComponent {
+  fname="";
+  lname="";
+  email="";
+}
